6113a34c7ecb3d140e1ef8366fc14427f735782e
Tako Schotanus <tako@ceylon-lang.org>
2018-03-19 12:51:03 +0100

    refactor(validator): Catalog validator now checks openshift yaml files

--- src/main/java/io/fabric8/launcher/booster/BoosterValidator.java
+++ src/main/java/io/fabric8/launcher/booster/BoosterValidator.java
@@ -11,14 +11,19 @@ import io.fabric8.launcher.booster.catalog.Booster;
 import io.fabric8.launcher.booster.catalog.BoosterCatalogService;
 import io.fabric8.launcher.booster.catalog.LauncherConfiguration;
 import io.fabric8.launcher.booster.catalog.spi.NativeGitBoosterCatalogPathProvider;
+import org.yaml.snakeyaml.Yaml;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.logging.Handler;
 import java.util.logging.Level;
@@ -55,10 +60,15 @@ class BoosterValidator {
         for (Booster b : boosters) {
             futures.add(b.content().whenComplete((path, throwable) -> {
                 if (throwable != null) {
-                    System.err.println("ERROR: Couldn't fetch Booster " + b.getName());
+                    System.err.println("ERROR: Couldn't fetch Booster " + b.getId());
                     errcnt.incrementAndGet();
                 } else {
-                    System.out.println("Fetched " + b.getName());
+                    System.out.println("Fetched " + b.getId() + " (" + b.getName() + ")");
+                    if (!validOpenshiftYamlFiles(b, path)) {
+                        errcnt.incrementAndGet();
+                    }
+                    System.out.flush();
+                    System.err.flush();
                 }
             }));
         }
@@ -70,6 +80,29 @@ class BoosterValidator {
             System.out.println("Done. " + errcnt.get() + " errors were encountered.");
             System.exit(1);
         }
+    }
+
+    private static boolean validOpenshiftYamlFiles(Booster booster, Path path) {
+        final Yaml yaml = new Yaml();
+        AtomicBoolean valid = new AtomicBoolean(true);
+        try {
+            Files.walk(path)
+                    .filter(p -> p.getParent().getFileName().toString().equals(".openshiftio"))
+                    .filter(p -> p.getFileName().toString().endsWith(".yaml") || p.getFileName().toString().endsWith(".yml"))
+                    .forEach(p -> {
+                        System.out.println("    Validating " + booster.getId() + " - " + path.relativize(p));
+                        try (BufferedReader reader = Files.newBufferedReader(p)) {
+                            yaml.loadAs(reader, Map.class);
+                        } catch (Exception e) {
+                            System.err.println("    ERROR: Parse error in " + booster.getId() + " - " + path.relativize(p) + ": " + e.getMessage());
+                            valid.set(false);
+                        }
+                    });
+        } catch (IOException e) {
+            System.err.println("    ERROR: IO error: " + booster.getId() + " - " + e.getMessage());
+            valid.set(false);
+        }
+        return valid.get();
     }
 
 }
